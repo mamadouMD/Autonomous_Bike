e9801532d770637cc20e953998883008352a8f29
rfan12 <ryan.ho.fan@gmail.com>
2019-05-09 16:47:31 -0400

    Did direction stuff

--- TowardsPoint/coordinate_testing.py
+++ TowardsPoint/coordinate_testing.py
@@ -1,12 +1,14 @@
 import coordinate_manipulation
 import geopy
+from geopy import distance
 
+coordm = coordinate_manipulation.CoordinateManipulation()
 
 startcoord = geopy.point.Point(38.991485,-76.937277)
 destinationcoord = geopy.point.Point(38.992615,-76.937294)
 
-bearing = coordm.bearing(currcoord, destinationcoord)
-distance = coordm.bearing(currcoord, destinationcoord)
+bearing = coordm.bearing(startcoord, destinationcoord)
+distance = distance.distance(startcoord, destinationcoord).meters
 
-print("This is the bike bearing: ", bike_heading)
-print("This is the destination heading: ", bearing)
+print("This is the distance: ", distance)
+print("This is the heading: ", bearing)
